6903fa9cfef9e33927e42fd62a82d6e7b9f6e061
rahmatulloh-tk <rahmatullohannafititiankasih@gmail.com>
2025-03-09 21:04:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Public/**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
